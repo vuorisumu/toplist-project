e2e9d8ea85276d7332fc6f74c41121593ffcd16b
Sumu Vuori <sumu.vuori@tuni.fi>
2024-01-05 03:43:00 +0200

    Add two empty views to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Main from "./components/Main.jsx";
+import NewTemplate from "./components/NewTemplate.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Main</Link>
+            </li>
+            <li>
+              <Link to="/new-template">New template</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/new-template" element={<NewTemplate />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/NewTemplate.jsx
@@ -0,0 +1,9 @@
+function NewTemplate() {
+  return (
+    <>
+      <h1>New Template</h1>
+    </>
+  );
+}
+
+export default NewTemplate;
