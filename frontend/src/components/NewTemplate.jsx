58a8e215f2bf7825e5d9c5be5b358b753c304128
Sumu Vuori <sumu.vuori@tuni.fi>
2024-01-07 16:13:30 +0200

    Set data for template creation

--- frontend/src/components/NewTemplate.jsx
+++ frontend/src/components/NewTemplate.jsx
@@ -1,25 +1,144 @@
+import { useState } from "react";
+
 function NewTemplate() {
+  const [templateName, setTemplateName] = useState("");
+  const [description, setDescription] = useState("");
+  const [creatorName, setCreatorName] = useState("");
+  const [items, setItems] = useState([""]);
+  const [tags, setTags] = useState([""]);
+  const [editKey, setEditKey] = useState("");
+
+  // add new item
+  const addItem = () => {
+    if (items[items.length - 1].trim() !== "") {
+      setItems([...items, ""]);
+    }
+  };
+
+  // add new tag
+  const addTag = () => {
+    if (tags[tags.length - 1].trim() !== "") {
+      setTags([...tags, ""]);
+    }
+  };
+
+  // edit an item
+  const handleItemEdits = (index, value) => {
+    const updatedItems = [...items];
+    updatedItems[index] = value;
+    setItems(updatedItems);
+  };
+
+  // edit a tag
+  const handleTagEdits = (index, value) => {
+    const updatedTags = [...tags];
+    updatedTags[index] = value;
+    setTags(updatedTags);
+  };
+
+  // delete item from index
+  const deleteItem = (index) => {
+    const updatedItems = items.filter((_, i) => i !== index);
+    setItems(updatedItems);
+  };
+
+  // delete tag from index
+  const deleteTag = (index) => {
+    const updatedTags = tags.filter((_, i) => i !== index);
+    setTags(updatedTags);
+  };
+
   return (
     <>
       <h1>New Template</h1>
       <div>
         <h2>Template info</h2>
         <label>Template name: </label>
-        <input type="text" placeholder="New Template" />
+        <input
+          type="text"
+          placeholder="New Template"
+          value={templateName}
+          onChange={(e) => setTemplateName(e.target.value)}
+        />
         <br />
         <label>Template description: </label>
-        <textarea placeholder="Description" />
+        <textarea
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <br />
         <label>Creator name: </label>
-        <input type="text" placeholder="Creator" />
+        <input
+          type="text"
+          placeholder="Creator"
+          value={creatorName}
+          onChange={(e) => setCreatorName(e.target.value)}
+        />
       </div>
 
       <div>
         <h2>Template items</h2>
-        <input type="text" placeholder="New item" />
-        <br />
-        <button type="button">+</button>
+        <ul>
+          {items.map((i, index) => (
+            <li key={"item" + index}>
+              <input
+                type="text"
+                placeholder="List item"
+                value={i}
+                onChange={(e) => handleItemEdits(index, e.target.value)}
+              />
+              {index !== items.length - 1 ? (
+                <button type="button" onClick={() => deleteItem(index)}>
+                  Delete
+                </button>
+              ) : (
+                <button type="button" onClick={addItem}>
+                  Add
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
+
+      <div>
+        <h2>Tags</h2>
+        <ul>
+          {tags.map((i, index) => (
+            <li key={"tag" + index}>
+              <input
+                type="text"
+                placeholder="Tag"
+                value={i}
+                onChange={(e) => handleTagEdits(index, e.target.value)}
+              />
+              {index !== tags.length - 1 ? (
+                <button type="button" onClick={() => deleteTag(index)}>
+                  Delete
+                </button>
+              ) : (
+                <button type="button" onClick={addTag}>
+                  Add
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <label>Edit key: </label>
+        <input
+          type="text"
+          placeholder="Edit key"
+          value={editKey}
+          onChange={(e) => setEditKey(e.target.value)}
+        />
+      </div>
+
+      <button type="button">Create</button>
+      <button type="button">Reset</button>
     </>
   );
 }
