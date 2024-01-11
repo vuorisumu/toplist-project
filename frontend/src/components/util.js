5604d43764d6e1e0ed68705b4250cf0e556e8eff
Sumu Vuori <sumu.vuori@tuni.fi>
2024-01-11 16:23:31 +0200

    Make utility function from fetching user id

--- frontend/src/components/util.js
+++ frontend/src/components/util.js
@@ -1,3 +1,5 @@
+import { fetchTagByName, addNewTag, addNewUser, fetchUserByName } from "./api";
+
 // get current time
 export const getLocalTime = () => {
   const timeNow = new Date();
@@ -19,4 +21,48 @@ export const checkAdminStatus = () => {
   const storedRole = localStorage.getItem("role");
 
   return storedAuth && storedRole === "admin";
+};
+
+// convert tag names to tag ids
+export const getTagNumbers = async (tagNames) => {
+  const tagNumbers = [];
+  try {
+    await Promise.all(
+      tagNames.map(async (t) => {
+        const fetchedTag = await fetchTagByName(t.trim());
+        let newId;
+        if (fetchedTag.length > 0) {
+          // tag already exists in database
+          newId = fetchedTag[0].id;
+        } else {
+          // add new tag to database
+          const newTag = {
+            name: t,
+          };
+          const newTagRes = await addNewTag(newTag);
+          newId = newTagRes.id;
+        }
+        tagNumbers.push(newId);
+      })
+    );
+    return tagNumbers;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getUserId = async (username) => {
+  const fetchedUser = await fetchUserByName(username.trim());
+  if (fetchedUser.length > 0) {
+    // use the id of user that already exists
+    return fetchedUser[0].user_id;
+  } else {
+    // add new user that's not already in database
+    const newUser = {
+      user_name: username,
+    };
+
+    const newUserResponse = await addNewUser(newUser);
+    return newUserResponse.id;
+  }
 };
