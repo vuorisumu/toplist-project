f5763c72487ccbd8ae6b25880cc729b2543f0f06
Sumu Vuori <sumu.vuori@tuni.fi>
2024-01-17 19:33:16 +0200

    Add description to main view

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -9,7 +9,16 @@ function Main() {
     <div className="container">
       <h1>Listmaker 9000</h1>
       <div className="pageInfo">
-        <p>Page description etc here</p>
+        <p>
+          Make your own top list using the templates provided by our passionate
+          top list template creators, explain your choices when you feel like
+          it, and browse through the top lists made by other creators.
+        </p>
+        <p>
+          Click on the template name to start ranking your own top list using
+          the chosen template. And don't worry - if you feel like the template
+          is missing an item, you can always add your own items too!
+        </p>
       </div>
       <Templates />
     </div>
