ac9763ee087b558dc1aa993b058ae6b2956e4d15
Sumu Vuori <vuori.sumu@gmail.com>
2024-07-20 02:26:21 +0300

    remove unused code

--- frontend/src/components/EditTemplate.jsx
+++ frontend/src/components/EditTemplate.jsx
@@ -5,7 +5,6 @@ import ButtonPrompt from "./ButtonPrompt";
 import { formatData } from "../util/dataHandler";
 import { isCreatorOfTemplate } from "../util/permissions";
 import { getCategories } from "../util/storage";
-import Dropdown from "./Dropdown";
 import { fetchTemplateById } from "../api/templates";
 import { updateTemplate, deleteTemplate } from "../api/templates";
 import TemplateData from "./TemplateData";
@@ -21,15 +20,13 @@ import TemplateData from "./TemplateData";
 function EditTemplate() {
   const [template, setTemplate] = useState(null);
   const categories = useRef([]);
-  // const [categories, setCategories] = useState(null);
-  const [chosenCategory, setChosenCategory] = useState("Uncategorized");
   const [canEdit, setCanEdit] = useState(false);
   const [loading, setLoading] = useState(true);
   const [notFound, setNotFound] = useState(false);
-  const [errors, setErrors] = useState([]);
   const location = useLocation();
   const navigate = useNavigate();
   const templateId = parseInt(location.pathname.replace("/edit-template/", ""));
+
   if (isNaN(templateId)) {
     console.error("Invalid templateId: ", templateId);
   }
@@ -87,130 +84,7 @@ function EditTemplate() {
     const tempData = data;
     const tempItems = data.items;
     tempData.items = tempItems;
-    if (data.category) {
-      const categoryName = categories.current
-        .filter((category) => category.id === data.category)
-        .map((category) => {
-          return category.name;
-        });
-      setChosenCategory(categoryName[0]);
-    }
-
     setTemplate(tempData);
-  };
-
-  /**
-   * Updates the template name
-   *
-   * @param {string} newName - new name for the template
-   */
-  const updateTemplateName = (newName) => {
-    setTemplate((prev) => ({
-      ...prev,
-      name: newName,
-    }));
-  };
-
-  /**
-   * Updates the template description
-   *
-   * @param {string} newDesc - new description for the template
-   */
-  const updateDescription = (newDesc) => {
-    setTemplate((prev) => ({
-      ...prev,
-      description: newDesc,
-    }));
-  };
-
-  /**
-   * Updates the name of an item at a specified index
-   *
-   * @param {string} newName - new item name
-   * @param {number} index - index of the item to be renamed
-   */
-  const updateItemName = (newName, index) => {
-    const newItems = template.items;
-    newItems[index].item_name = newName;
-    setTemplate((prevTemp) => ({
-      ...prevTemp,
-      items: newItems,
-    }));
-  };
-
-  /**
-   * Deletes an item from a specified index
-   *
-   * @param {number} index - index of the item to be deleted
-   */
-  const deleteItem = (index) => {
-    const newItems = template.items.filter((_, i) => i !== index);
-    setTemplate((prevTemp) => ({
-      ...prevTemp,
-      items: newItems,
-    }));
-  };
-
-  /**
-   * Adds a new input field for items if the latest item is not empty
-   * Does nothing if the last item is empty
-   */
-  const addNewField = () => {
-    const lastItem = template.items[template.items.length - 1];
-    if (lastItem.item_name.trim() === "") {
-      return;
-    }
-    setTemplate((prevTemp) => ({
-      ...prevTemp,
-      items: [...prevTemp.items, { item_name: "" }],
-    }));
-  };
-
-  /**
-   * Updates the category of the template.
-   *
-   * @param {string} newCategory - Chosen category
-   */
-  const updateCategory = (newCategory) => {
-    const newCategoryId = categories.current
-      .filter((category) => category.name === newCategory)
-      .map((category) => {
-        return category.id;
-      });
-    setTemplate((prevTemp) => ({
-      ...prevTemp,
-      category: newCategoryId[0],
-    }));
-  };
-
-  /**
-   * Checks if the created template meets the minimum requirements
-   * for saving to the database
-   *
-   * @returns true if requirements have been met, false if the name field
-   * is empty or if the template has less than five items
-   */
-  const meetsRequirements = () => {
-    const tempErrors = [];
-    const hasName = template.name.trim() !== "";
-    if (!hasName) {
-      tempErrors.push("Template must have a name");
-      document.getElementById("tempName").classList.add("error");
-    } else {
-      document.getElementById("tempName").classList.remove("error");
-    }
-
-    const enoughItems =
-      template.items.filter((i) => i.item_name.trim() !== "").length >= 5;
-    if (!enoughItems) {
-      tempErrors.push("Template must have at least 5 items");
-      document.getElementById("tempItems").classList.add("error");
-    } else {
-      document.getElementById("tempItems").classList.remove("error");
-    }
-
-    setErrors(tempErrors);
-    return hasName && enoughItems;
   };
 
   /**
