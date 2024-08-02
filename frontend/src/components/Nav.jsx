eef3e2ef5fe844bb7ce511503469b958b8327c7a
Sumu Vuori <vuori.sumu@gmail.com>
2024-08-03 02:22:19 +0300

    refactor navigation code

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -18,81 +18,9 @@ function Nav() {
     document.getElementById("navLogin").classList.toggle("active");
   };
 
-  if (isMobile) {
+  function Links() {
     return (
       <>
-        <nav className="mobile">
-          <ul>
-            <li>
-              <NavLink to="/" id="logoCont">
-                <img src={logo} id="logo" />
-                <span>Listmaker 9000</span>
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="mobileNav">
-          <Login isFixed={true} />
-          <ul>
-            <li>
-              <NavLink to="/templates">
-                <span className="material-symbols-outlined icon">
-                  view_list
-                </span>
-                <span className="linkName">Templates</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/toplists">
-                <span className="material-symbols-outlined icon">
-                  format_list_numbered
-                </span>
-                <span className="linkName">Top lists</span>
-              </NavLink>
-            </li>
-            {user && (
-              <li>
-                <NavLink to="/new-template">
-                  <span className="material-symbols-outlined icon">
-                    list_alt_add
-                  </span>
-                  <span className="linkName">New template</span>
-                </NavLink>
-              </li>
-            )}
-            <li>
-              <ThemeButton />
-            </li>
-            <li>
-              <div
-                className="toggleLogin loginIcon"
-                onClick={toggleLogin}
-                id="navLogin"
-              >
-                <span className="material-symbols-outlined icon loginIcon">
-                  {user ? "lock_open" : "lock"}
-                </span>
-                <span className="linkName loginIcon">
-                  {user ? "Logged in" : "Login"}
-                </span>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </>
-    );
-  }
-
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" id="logoCont">
-            <img src={logo} id="logo" />
-            <span>Listmaker 9000</span>
-          </NavLink>
-        </li>
         <li>
           <NavLink to="/templates">
             <span className="material-symbols-outlined icon">view_list</span>
@@ -134,6 +62,44 @@ function Nav() {
             </span>
           </div>
         </li>
+      </>
+    );
+  }
+
+  if (isMobile) {
+    return (
+      <>
+        <nav className="mobile">
+          <ul>
+            <li>
+              <NavLink to="/" id="logoCont">
+                <img src={logo} id="logo" />
+                <span>Listmaker 9000</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="mobileNav">
+          <Login isFixed={true} />
+          <ul>
+            <Links />
+          </ul>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" id="logoCont">
+            <img src={logo} id="logo" />
+            <span>Listmaker 9000</span>
+          </NavLink>
+        </li>
+        <Links />
       </ul>
       <Login isFixed={true} />
     </nav>
