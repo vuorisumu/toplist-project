a1fe586d30c20384118f93ab4725f57dfca60760
Sumu Vuori <vuori.sumu@gmail.com>
2024-11-15 19:59:53 +0200

    update: display categories

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { getCategories } from "../util/storage";
+
 function Categories() {
+  const [categories, setCategories] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!categories) {
+      getCategories()
+        .then((data) => {
+          setCategories(data);
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
   return (
     <div className="container">
       <h1>Categories</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        categories && (
+          <ul>
+            {categories.map((c) => (
+              <li key={`c${c.id}`}>{c.name}</li>
+            ))}
+          </ul>
+        )
+      )}
     </div>
   );
 }
