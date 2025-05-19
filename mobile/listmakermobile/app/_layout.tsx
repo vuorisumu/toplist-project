fdd5f762822a875b54b5058f49e73cc568f5a2d5
Sumu Vuori <sumu.vuori@tuni.fi>
2025-05-19 18:35:44 +0300

    Reset mobile project test

--- mobile/listmakermobile/app/_layout.tsx
+++ mobile/listmakermobile/app/_layout.tsx
@@ -1,32 +1,5 @@
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
-
-import { Colors } from "@/constants/Colors";
-import AppContext from "@/hooks/useAppContext";
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
-    const colors = Colors[colorScheme ?? "light"];
-
-    const [loaded] = useFonts({
-        SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    });
-
-    if (!loaded) {
-        // Async font loading only occurs in development.
-        return null;
-    }
-
-    return (
-        <AppContext.Provider value={{ colors }}>
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="+not-found" />
-            </Stack>
-            <StatusBar style="auto" />
-        </AppContext.Provider>
-    );
+  return <Stack />;
 }
--- mobile/listmakermobile/hooks/useAppContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext, useContext } from "react";
-const AppContext = createContext<any>({});
-export const useAppContext = () => useContext(AppContext);
-export default AppContext;
