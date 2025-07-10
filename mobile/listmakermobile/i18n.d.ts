af1d6baa0f7161c60fe1bd492f85daef6cb58f4a
Sumu Vuori <sumu.vuori@tuni.fi>
2025-07-10 11:40:07 +0300

    Add words to translation

--- mobile/listmakermobile/i18n.d.ts
+++ mobile/listmakermobile/i18n.d.ts
@@ -8,6 +8,13 @@ declare module 'i18next' {
             yes: string;
             no: string;
         };
+        templates: {
+            new: string;
+            info: string;
+            name: string;
+            description: string;
+            category: string;
+        };
       };
     };
   }
