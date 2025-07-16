ff9313eeaf1f64cff686b9eaceae2c90dc4a5956
Sumu Vuori <sumu.vuori@tuni.fi>
2025-07-16 18:53:22 +0300

    remove placeholder text

--- mobile/listmakermobile/app/template/new.tsx
+++ mobile/listmakermobile/app/template/new.tsx
@@ -1,11 +1,9 @@
 import EditableTemplate from "@/components/blocks/EditableTemplate";
-import { Paragraph } from "@/components/Paragraph";
 import ViewContainer from "@/components/ViewContainer";
 
 export default function NewTemplate() {
     return (
         <ViewContainer>
-            <Paragraph>Creating new template</Paragraph>
             <EditableTemplate />
         </ViewContainer>
     );
