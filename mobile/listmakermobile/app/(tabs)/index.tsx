98317fb2c6935cef26c4d5ae1d84415126feda27
Sumu Vuori <sumu.vuori@tuni.fi>
2025-07-27 13:47:39 +0300

    disable warning

--- mobile/listmakermobile/app/(tabs)/index.tsx
+++ mobile/listmakermobile/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { fetchTemplates } from "@/api/templates";
 import LoadingArea from "@/components/blocks/LoadingArea";
 import TemplateList from "@/components/blocks/TemplateList";
