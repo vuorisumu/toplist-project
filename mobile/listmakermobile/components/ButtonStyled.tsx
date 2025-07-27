c84ff9bfdcfb0671f24f0aab2b2af8798e126415
Sumu Vuori <sumu.vuori@tuni.fi>
2025-07-27 11:44:29 +0300

    add custimization to button

--- mobile/listmakermobile/components/ButtonStyled.tsx
+++ mobile/listmakermobile/components/ButtonStyled.tsx
@@ -1,23 +1,53 @@
 import { useAppContext } from "@/utils/AppContext";
 import { Colors } from "@/utils/Colors";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { ComponentProps } from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
 
 type Props = {
     title: string;
     onPress: () => void;
+    icon?: ComponentProps<typeof MaterialIcons>["name"];
     alt?: boolean;
+    size?: number;
 };
 
-export default function ButtonStyled({ title, onPress, alt = false }: Props) {
+export default function ButtonStyled({
+    title,
+    onPress,
+    icon,
+    alt = false,
+    size,
+}: Props) {
     const { theme } = useAppContext();
     const background = alt ? Colors[theme].mid : Colors[theme].icon;
+    const big = size && size > 20;
 
     return (
         <Pressable
-            style={[{ backgroundColor: background }, styles.button]}
+            style={[
+                { backgroundColor: background, padding: big ? 6 : 4 },
+                styles.button,
+            ]}
             onPress={onPress}
         >
-            <Text style={[{ color: Colors[theme].white }, styles.text]}>
+            {icon && (
+                <MaterialIcons
+                    name={icon}
+                    size={size || 20}
+                    color={Colors[theme].white}
+                />
+            )}
+            <Text
+                style={[
+                    {
+                        color: Colors[theme].white,
+                        fontSize: big ? size - 4 : 16,
+                        fontWeight: big ? 600 : 400,
+                    },
+                    styles.text,
+                ]}
+            >
                 {title}
             </Text>
         </Pressable>
@@ -27,10 +57,12 @@ export default function ButtonStyled({ title, onPress, alt = false }: Props) {
 const styles = StyleSheet.create({
     button: {
         borderRadius: 20,
-        padding: 4,
+        flexDirection: "row",
+        gap: 10,
+        justifyContent: "center",
+        alignItems: "center",
     },
     text: {
         textAlign: "center",
-        fontSize: 16,
     },
 });
